4faf6ab249dba8c2db561af8b599625df39337c6
Carlos Segura <cseguramail@gmail.com>
2023-12-11 11:48:22 +0100

    Adding tcl mobility file to RealSce

--- scenarios/examples/Realistic_scenario/RealSce/RealSce.sh
+++ scenarios/examples/Realistic_scenario/RealSce/RealSce.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp scratch/RealSce/Real_model-attributes.txt .
+cp scratch/RealSce/satt_6c_r_mob_5000_ped_veh_r2.tcl ./scratch/
 CXXFLAGS="-O3" ./waf configure -d debug --enable-examples --enable-tests
 ./waf build
 
