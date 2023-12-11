30fef3cdbbd85fa7381e487c04871999b45f64f6
Carlos Segura <cseguramail@gmail.com>
2023-12-11 12:21:59 +0100

    Separating tcl mobility files from the docker image into the example code

--- scenarios/examples/Joint_Power_CIO_optimization/Power_CIO/Power_CIO.sh
+++ scenarios/examples/Joint_Power_CIO_optimization/Power_CIO/Power_CIO.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CXXFLAGS="-O3" ./waf configure -d debug --enable-examples --enable-tests
 ./waf build
-
+cp scratch/Power_CIO/satt_6c_r_mob_5000_ped_veh_r2.tcl ./scratch/
 
 for ((i=1;i<=10000;i++))
 do
