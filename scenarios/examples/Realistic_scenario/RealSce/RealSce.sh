24ad7879e32ab40758cd773c46da773d90860912
Carlos Segura <cseguramail@gmail.com>
2023-12-22 16:57:52 +0100

    Updating scripts for RealSce

--- scenarios/examples/Realistic_scenario/RealSce/RealSce.sh
+++ scenarios/examples/Realistic_scenario/RealSce/RealSce.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cp scratch/RealSce/Real_model-attributes.txt .
 cp scratch/RealSce/satt_6c_r_mob_5000_ped_veh_r2.tcl ./scratch/
-CXXFLAGS="-O3" ./waf configure -d debug --enable-examples --enable-tests
-./waf build
+
+./ns3 configure --build-profile=optimized --enable-examples --enable-tests  --enable-mpi --enable-python
+./ns3 build
 
 
 for i in {1..250}
 do
-	./waf --run "scratch/RealSce/RealSce --RunNum=$(($i))"
+	./ns3 run "scratch/RealSce/LTE_Environment --RunNum=$(($i))"
 done
