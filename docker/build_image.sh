def277a2ca61a144e3bbeeb93fb9bcc1c76285bf
Carlos Segura <cseguramail@gmail.com>
2024-02-01 15:51:18 +0100

    updating docker with "refactor" version tag using refactor/lte_sim_ref ns3 branch

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,7 +1,7 @@
-docker build -t ns3-dev-mobility:latest ./
+docker build -t ns3-dev-mobility:refactor ./
 
 
-containerId=$(docker create ns3-dev-mobility:latest)
+containerId=$(docker create ns3-dev-mobility:refactor)
 docker cp "$containerId":/usr/ns-allinone-3.30/ns-3-mobility/contrib/opengym/model/ns3gym/dist/ns3gym-0.1.0-py3-none-any.whl ./
 docker rm "$containerId"
 
